4eacb279754fa7e205c5d5a63b2f4606f7657c8c
Pedro Coelho <pedrooliveira.ocb@gmail.com>
2026-03-13 12:49:24 -0300

    Aula 09 Eventos onclick e submit

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,26 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List'
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
-  const nome = "Maria"
   return (
     <div className="App">
-      <h1>Testando css</h1>
-      <SayMyName nome="Pedro" />
-      <SayMyName nome={nome} />
-
-      <Pessoa
-        nome="Rodrigo"
-        idade="20"
-        profissao="programador"
-        foto="https://via.placeholder.com/150" />
-      <Frase/>
-      <List/>
+      <h1>Testando Eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2"/>
+      
+      <Form/>
     </div>
   );
 }
