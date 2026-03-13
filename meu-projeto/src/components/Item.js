ebfff12f3f9e286f70001e832de80df9bec173ea
Pedro Coelho <pedrooliveira.ocb@gmail.com>
2026-03-12 22:27:14 -0300

    Aula 08 propTypes e defaultProps

--- meu-projeto/src/components/Item.js
+++ meu-projeto/src/components/Item.js
@@ -1,8 +1,14 @@
-function Item(props){
+import PropTypes from 'prop-types'
+
+function Item({ marca = "sem marca", ano_lancamento = 0 }) {
     return(
-        <>
-            <li>{props.marca}</li>
-        </>
+        <li>{marca} - {ano_lancamento}</li>
     )
 }
+
+Item.propTypes = {
+    marca: PropTypes.string,
+    ano_lancamento: PropTypes.number
+}
+
 export default Item
--- meu-projeto/src/components/List.js
+++ meu-projeto/src/components/List.js
@@ -4,9 +4,11 @@ function List(){
     <>
         <h1>Minha lista</h1>
         <ul>
-            <Item marca="Ferrari" />
-            <Item marca="Fiat"/>
-            <Item marca="Renault"/>
+            <Item marca="Ferrari" ano_lancamento={2000} />
+            <Item marca="Fiat"  ano_lancamento={1995}/>
+            <Item marca="Renault"  ano_lancamento={2012}/>
+            <Item />
+            
         </ul>
     </>
  )   
