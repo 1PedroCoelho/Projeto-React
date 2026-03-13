eda2e922ff4d24499292aa296f7bd926a9c5267b
Pedro Coelho <pedrooliveira.ocb@gmail.com>
2026-03-13 15:45:28 -0300

    Aula 11 Passar eventos

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Evento from './components/Evento';
 import Form from './components/Form';
@@ -7,8 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Testando Eventos</h1>
-      <Evento numero="1"/>
-      <Evento numero="2"/>
+      <Evento/>
       
       <Form/>
     </div>
